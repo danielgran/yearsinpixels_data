d50a2747c8c1de28bc0b2a15f4bb7b5a52eae483
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-11-24 06:02:14 +0100

    Added: UserMap with the corresponding test class

--- src/yearsinpixels_data/EntityMap/UserMap.py
+++ src/yearsinpixels_data/EntityMap/UserMap.py
@@ -4,4 +4,52 @@ from yearsinpixels_data.EntityMap.EntityMap import EntityMap
 class UserMap(EntityMap):
     def get_common_name(self):
         return "user"
-    
+
+    @property
+    def email(self):
+        return "email"
+
+    @property
+    def email_verified(self):
+        return "email_verified"
+
+    @property
+    def name_first(self):
+        return "name_first"
+
+    @property
+    def name_last(self):
+        return "name_last"
+
+    @property
+    def gender(self):
+        return "gender"
+
+    @property
+    def password(self):
+        return "password"
+
+    @property
+    def password_lastupdate(self):
+        return "password_lastupdate"
+
+    @property
+    def enabled(self):
+        return "enabled"
+
+    @property
+    def twofatoken(self):
+        return "twofatoken"
+
+    @property
+    def last_login(self):
+        return "last_login"
+
+    @property
+    def modified(self):
+        return "modified"
+
+    @property
+    def created(self):
+        return "created"
+
--- test/EntityMap/test_UserMap.py
+++ test/EntityMap/test_UserMap.py
@@ -8,3 +8,12 @@ class UserMapTest(unittest.TestCase):
     def test_usermap(self):
         usermap = UserMap()
         self.assertTrue(issubclass(UserMap, EntityMap) and len(usermap.get_common_name()) > 0)
+
+    def test_attrs(self):
+        usermap = UserMap()
+        for var in dir(usermap):
+            if var.startswith("_"):
+                continue
+
+            return_value_from_property = getattr(usermap, var)
+            self.assertTrue(isinstance(return_value_from_property, str) and len(return_value_from_property) > 0)
