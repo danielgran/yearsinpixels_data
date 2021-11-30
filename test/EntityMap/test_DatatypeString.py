6cce5ec58dc640e2e576f6a17b837f6079d1ec33
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-11-30 05:56:36 +0100

    Refactored

--- test/EntityMap/test_DatatypeString.py
+++ test/EntityMap/test_DatatypeString.py
@@ -2,14 +2,13 @@ import unittest
 from abc import ABC
 
 from yearsinpixels_data.EntityMap.Datatype import Datatype
-from yearsinpixels_data.EntityMap.DatatypeInteger import DatatypeInteger
-from yearsinpixels_data.EntityMap.DatatypeString import DatatypeString
+from yearsinpixels_data.EntityMap.DatatypeDatetime import DatatypeString
 
 
-class DatatypeIntegerTest(unittest.TestCase):
+class DatatypeStringTest(unittest.TestCase):
     def test_is_there(self):
         self.assertIsNotNone(DatatypeString)
 
     def test_inheritance(self):
-        self.assertTrue(issubclass(DatatypeInteger, ABC))
-        self.assertTrue(issubclass(DatatypeInteger, Datatype))
+        self.assertTrue(issubclass(DatatypeString, ABC))
+        self.assertTrue(issubclass(DatatypeString, Datatype))
