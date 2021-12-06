ac5aadb891f52e677197f6c9be6757ce4968d3c0
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-12-06 10:13:18 +0100

    Updated: lib dependencies script

--- scripts/update-lib-dependencies.sh
+++ scripts/update-lib-dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # This file must get called out of the project root.
-pip install --upgrade --force-reinstall ./lib/yearsinpixels_business-0.0.1-py3-none-any.whl
+pip uninstall -y yearsinpixels-business
+
+pip install ./lib/yearsinpixels_business-0.0.1-py3-none-any.whl
